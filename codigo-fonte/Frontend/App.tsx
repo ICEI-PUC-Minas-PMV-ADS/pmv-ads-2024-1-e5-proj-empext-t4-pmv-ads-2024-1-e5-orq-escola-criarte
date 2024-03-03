f4e740e3d545ba59fd34f68901a3e9fb5a68e522
Rafael Souza <rafael.souza54@hotmail.com>
2024-03-03 19:59:30 -0300

    ajuste

--- codigo-fonte/Frontend/App.tsx
+++ codigo-fonte/Frontend/App.tsx
@@ -1,14 +1,8 @@
-import {NavigationContainer} from '@react-navigation/native';
-import Header from './src/components/Header';
-import Routes from './src/router'
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import LoginScreen from './src/screens/login';
-import HomeScreen from './src/screens/home';
-import Contacts from './src/screens/contacts';
-import Events from './src/screens/events';
+
 import MyStack from './src/router/stack';
 
 const Stack = createStackNavigator();
@@ -19,7 +13,5 @@ const MainStack = () => {
     <MyStack/>
   );
 }
-
-
 
 export default MainStack;
