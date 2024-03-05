43d3113b0d7ba2383084c66c28f2987a8529ecf5
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-03-05 15:58:36 -0300

    cache

---
+++ codigo-fonte/backend/InformativoOEC/InformativoOEC.Application/Models/InputModels/PostInputModel.cs
@@ -0,0 +1,13 @@
+﻿using InformativoOEC.Core.ValueObjects;
+
+namespace InformativoOEC.Application.Models.InputModels;
+public class PostInputModel
+{
+    public Content Content { get; set; }
+    public string ImageURL { get; set; }
+    public DateTime Date { get; set; }
+    public Address Address { get; set; }
+    public string Username { get; set; }
+
+    public Core.Entities.Post ToEntity => new(Content, ImageURL, Date, Address, Username);
+}
