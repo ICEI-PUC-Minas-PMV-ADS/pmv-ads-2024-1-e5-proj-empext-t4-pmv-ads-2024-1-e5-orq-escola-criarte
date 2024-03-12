5171f4d4f510baa0e57ea45bba548ef705be11d0
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-03-12 14:49:41 -0300

    cached

--- codigo-fonte/backend/InformativoOEC/InformativoOEC.Core/ValueObjects/Address.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace InformativoOEC.Core.ValueObjects;
-public class Address
-{
-    public string Street { get; set; }
-    public string Number { get; set; }
-    public string County { get; set; }
-}
