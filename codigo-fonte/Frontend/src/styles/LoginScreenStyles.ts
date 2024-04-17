2881a4364d6bda731ea9264edd73c6a6be592da8
PedrowiskDev <ph6304112@gmail.com>
2024-04-16 23:07:21 -0300

    Modal de (senha incorreta) ao tentar inserir um email ou senha incorretos

--- codigo-fonte/Frontend/src/styles/LoginScreenStyles.ts
+++ codigo-fonte/Frontend/src/styles/LoginScreenStyles.ts
@@ -24,6 +24,40 @@ const loginScreenStyles = StyleSheet.create({
   botaoCadastrar: {
     width: '80%',
   },
+
+  errorModalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+
+  errorModalContent: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+
+  errorModalText: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+
+  errorModalButton: {
+    marginTop: 10,
+    backgroundColor: '#6750a4',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  errorModalButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+
 });
 
 export default loginScreenStyles;
