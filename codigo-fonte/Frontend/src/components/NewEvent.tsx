3abf1bc1f9c5aacb21ee4be6e3a585d5e875ba2f
PedrowiskDev <ph6304112@gmail.com>
2024-04-25 23:58:13 -0300

    Função de crição de evento implementada

--- codigo-fonte/Frontend/src/components/NewEvent.tsx
+++ codigo-fonte/Frontend/src/components/NewEvent.tsx
@@ -3,27 +3,44 @@ import { Modal, View, Text, TextInput, Pressable, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { api } from '../config/authUtils';
 import styles from '../styles/ModalCreateEvent';
+import { getToken } from '../config/authUtils';
+import { jwtDecode } from 'jwt-decode';
 
 interface Props {
     visible: boolean;
     onClose: () => void;
 }
 
+interface UserData {
+    'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name': string;
+}
+
 export default function CreateEventModal({ visible, onClose }: Props) {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
-    const [address, setAddress] = useState('');
+    const [street, setStreet] = useState('');
+    const [number, setNumber] = useState('');
+    const [county, setCounty] = useState('');
     const [image, setImage] = useState<{ uri: string }>({ uri: '' });
-    const [date, setDate] = useState('');
+    const [username, setUsername] = React.useState<string>('');
 
-    const handleDateChange = (newDate: string) => {
-        if (/^[0-9/]*$/.test(newDate)) {
-            if (newDate.length === 2 || newDate.length === 5) {
-                newDate += '/';
+    React.useEffect(() => {
+        async function fetchUserData() {
+            try {
+                const token = await getToken();
+                if (token) {
+                    const decoded = jwtDecode<UserData>(token);
+                    setUsername(decoded['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name']);
+                } else {
+                    console.log('Token é nulo');
+                }
+            } catch (error) {
+                console.error("Erro ao obter os dados do usuário:", error);
             }
-            setDate(newDate);
-        }
-    };
+        };
+
+        fetchUserData();
+    }, []);
 
     const selectImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
@@ -40,26 +57,51 @@ export default function CreateEventModal({ visible, onClose }: Props) {
     };
 
     const handleSubmit = async () => {
-        try {
-            await api.post('/events', {
+        const event = {
+            content: {
                 title,
                 body,
-                date,
-                address,
-                image
-            });
+            },
+            imageURL: image.uri,
+            date: new Date().toISOString(),
+            address: {
+                street,
+                number,
+                county,
+            },
+            username: username,
+        };
+
+        console.log('Enviando o seguinte objeto para a API:', event);
+
+        try {
+            const response = await api.post('/posts', event);
+            if (response.status === 200) {
+
+                console.log('Evento criado com sucesso');
+
+                setTitle('');
+                setBody('');
+                setStreet('');
+                setNumber('');
+                setCounty('');
+                setImage({ uri: '' });
+            } else {
+                throw new Error('Falha na criação do evento');
+            }
             onClose();
-        } catch (error) {
-            console.error(error);
+        } catch (error: any) {
+            console.error('Error:', error.message);
         }
     };
 
     const handleCancel = () => {
         setTitle('');
         setBody('');
-        setAddress('');
+        setStreet('');
+        setNumber('');
+        setCounty('');
         setImage({ uri: '' });
-        setDate('');
 
         onClose();
     };
@@ -72,8 +114,9 @@ export default function CreateEventModal({ visible, onClose }: Props) {
                     <Text style={styles.title}>Criar Evento</Text>
                     <TextInput style={styles.input} placeholder="Título" value={title} onChangeText={setTitle} />
                     <TextInput style={styles.descriptionInput} placeholder="Descrição" value={body} onChangeText={setBody} multiline />
-                    <TextInput style={styles.input} placeholder="Data (dd/mm/aaaa)" value={date} onChangeText={handleDateChange} maxLength={10} />
-                    <TextInput style={styles.input} placeholder="Endereço" value={address} onChangeText={setAddress} />
+                    <TextInput style={styles.input} placeholder="Rua" value={street} onChangeText={setStreet} />
+                    <TextInput style={styles.input} placeholder="Número" value={number} onChangeText={setNumber} />
+                    <TextInput style={styles.input} placeholder="Condado" value={county} onChangeText={setCounty} />
                     <Pressable style={styles.button} onPress={selectImage}>
                         <Text style={styles.buttonText}>Selecionar Imagem</Text>
                     </Pressable>
--- codigo-fonte/Frontend/src/screens/events.tsx
+++ codigo-fonte/Frontend/src/screens/events.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Image, ImageBackground, Pressable } from "react-native";
+import { View, Text, Image, ImageBackground, Pressable, ScrollView, RefreshControl } from "react-native";
 import styles from '../styles/EventsScreenStyles';
 import globaStyles from '../styles/LoginScreenStyles';
 import { api, getToken } from '../config/authUtils';
@@ -22,32 +22,72 @@ interface Event {
 }
 
 interface UserData {
-  'http://schemas.microsoft.com/ws/2008/06/identity/claims/role': string;
+    'http://schemas.microsoft.com/ws/2008/06/identity/claims/role': string;
 }
 
 function Events() {
     const [events, setEvents] = useState<Event[]>([]);
     const [userRole, setUserRole] = useState<string>('');
     const [isModalVisible, setIsModalVisible] = useState(false);
+    const [refreshing, setRefreshing] = useState(false);
+
+    const fetchEvents = async () => {
+        try {
+            const response = await api.get<Event[]>('/newsfeed');
+            setEvents(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const fetchUserRole = async () => {
+        const token = await getToken();
+        if (token) {
+            const decoded = jwtDecode<UserData>(token);
+            setUserRole(decoded['http://schemas.microsoft.com/ws/2008/06/identity/claims/role']);
+        }
+    };
 
     useEffect(() => {
-        api.get<Event[]>('/newsfeed')
-            .then(response => setEvents(response.data))
-            .catch(error => console.error(error));
-
-        const fetchUserRole = async () => {
-            const token = await getToken();
-            if (token) {
-                const decoded = jwtDecode<UserData>(token);
-                setUserRole(decoded['http://schemas.microsoft.com/ws/2008/06/identity/claims/role']);
-            }
-        };
-
+        fetchEvents();
         fetchUserRole();
     }, []);
 
+    const closeModal = () => {
+        setIsModalVisible(false);
+        fetchEvents();
+    };
+
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        fetchEvents().then(() => setRefreshing(false));
+    }, []);
+
     return (
-        <ImageBackground source={require('../assets/background.png')} style={globaStyles.background}>
+        <View style={{flex: 1}}>
+            <ScrollView
+                contentContainerStyle={{ flexGrow: 1 }}
+                refreshControl={
+                    <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+                }
+            >
+                {events.map((event, index) => (
+                    <View key={index} style={styles.rectangle}>
+                        <View style={styles.row}>
+                            <Text style={styles.label}>{new Date(event.date).toLocaleDateString()}</Text>
+                            <Text style={styles.label}>{event.address.street}</Text>
+                            <Text style={styles.label}>{new Date(event.date).toLocaleTimeString()}</Text>
+                        </View>
+                        <View style={styles.eventDetails}>
+                            <Text style={styles.eventTitle}>{event.content.title}</Text>
+                            <Image
+                                source={{ uri: event.imageURL }}
+                                style={styles.eventImage}
+                            />
+                        </View>
+                    </View>
+                ))}
+            </ScrollView>
             {userRole === 'Admin' && (
                 <Pressable
                     style={styles.createEventButton}
@@ -59,25 +99,10 @@ function Events() {
                     <Text style={styles.createEventButtonText}>Criar Evento</Text>
                 </Pressable>
             )}
-            <CreateEventModal visible={isModalVisible} onClose={() => setIsModalVisible(false)} />
-            {events.map((event, index) => (
-                <View key={index} style={styles.rectangle}>
-                    <View style={styles.row}>
-                        <Text style={styles.label}>{new Date(event.date).toLocaleDateString()}</Text>
-                        <Text style={styles.label}>{event.address.street}</Text>
-                        <Text style={styles.label}>{new Date(event.date).toLocaleTimeString()}</Text>
-                    </View>
-                    <View style={styles.eventDetails}>
-                        <Text style={styles.eventTitle}>{event.content.title}</Text>
-                        <Image
-                            source={{ uri: event.imageURL }}
-                            style={styles.eventImage}
-                        />
-                    </View>
-                </View>
-            ))}
-        </ImageBackground>
+            <CreateEventModal visible={isModalVisible} onClose={closeModal} />
+        </View>
     );
+    
 }
 
 export default Events;
