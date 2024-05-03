1272117664f88d4a02b5b98867d40a52f28beff4
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-05-03 10:56:58 -0300

    feat: implementando estrutura de dados do endpoint de news

---
+++ codigo-fonte/Backend/InformativoOEC/InformativoOEC.Core/Repositories/INewsRepository.cs
@@ -0,0 +1,7 @@
+﻿using InformativoOEC.Core.Entities;
+
+namespace InformativoOEC.Core.Repositories;
+public interface INewsRepository
+{
+    Task AddAsync(News news);
+}
---
+++ codigo-fonte/Backend/InformativoOEC/InformativoOEC.Infra/Persistence/Configurations/NewsConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using InformativoOEC.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InformativoOEC.Infra.Persistence.Configurations;
+public class NewsConfiguration : IEntityTypeConfiguration<News>
+{
+    public void Configure(EntityTypeBuilder<News> builder)
+    {
+        builder.HasKey(n => n.Id);
+
+        builder.Property(n => n.Title).HasMaxLength(150).IsRequired();
+        builder.Property(n => n.Description).HasMaxLength(1000).IsRequired();
+        builder.Property(n => n.ImageURL).HasMaxLength(2000).IsRequired();
+    }
+}
