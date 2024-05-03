0d1a4627d0981fcd6749ad7020e808aa211341d7
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-05-03 13:02:26 -0300

    feat: implementando endpoint create news

---
+++ codigo-fonte/Backend/InformativoOEC/InformativoOEC.API/Controllers/NewsController.cs
@@ -0,0 +1,26 @@
+﻿using InformativoOEC.Application.Models.InputModels;
+using InformativoOEC.Application.Services.News;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InformativoOEC.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class NewsController : ControllerBase
+{
+    private readonly INewsService _newsService;
+    public NewsController(INewsService newsService)
+    {
+        _newsService = newsService;
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> Create([FromBody] NewsInputModel input)
+    {
+        var result = await _newsService.Create(input);
+
+        return Ok(result.Id);
+    }
+
+}
