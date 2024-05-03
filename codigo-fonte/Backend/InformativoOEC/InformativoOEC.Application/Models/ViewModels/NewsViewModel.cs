fe330b2e9202abf2970aedb2ad653af360dff20e
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-05-03 11:25:02 -0300

    feat: implementando models do endpoint de news

---
+++ codigo-fonte/Backend/InformativoOEC/InformativoOEC.Application/Models/ViewModels/NewsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using InformativoOEC.Core.Entities;
+
+namespace InformativoOEC.Application.Models.ViewModels;
+public class NewsViewModel
+{
+    public NewsViewModel(News news)
+    {
+        Title = news.Title;
+        Description = news.Description;
+        ImageURL = news.ImageURL;
+        PostDateTime = news.CreatedAt;
+    }
+
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ImageURL { get; set; }
+    public DateTime? PostDateTime { get; set; }
+}
