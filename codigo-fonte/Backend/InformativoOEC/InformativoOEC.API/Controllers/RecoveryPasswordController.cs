a8c116eada3042047f22ac528d3f88d9656f88df
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-05-24 14:46:21 -0300

    feat: implementando funcionalidade de atualizar a senha via recovery password

--- codigo-fonte/Backend/InformativoOEC/InformativoOEC.API/Controllers/RecoveryPasswordController.cs
+++ codigo-fonte/Backend/InformativoOEC/InformativoOEC.API/Controllers/RecoveryPasswordController.cs
@@ -1,5 +1,6 @@
 ﻿using InformativoOEC.Application.Models.InputModels;
 using InformativoOEC.Application.Services.RecoveryPassword;
+using InformativoOEC.Application.Services.User;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,13 +10,16 @@ namespace InformativoOEC.API.Controllers;
 public class RecoveryPasswordController : ControllerBase
 {
     private readonly IRecoveryPasswordService _service;
+    private readonly IUserService _userService;
 
-    public RecoveryPasswordController(IRecoveryPasswordService service)
+    public RecoveryPasswordController(IRecoveryPasswordService service, IUserService userService)
     {
         _service = service;
+        _userService = userService;
     }
 
     [HttpPost]
+    [Route("email-recovery")]
     [AllowAnonymous]
     public async Task<IActionResult> EmailRecovery(RecoveryPasswordInputModel input)
     {
@@ -25,11 +29,22 @@ public class RecoveryPasswordController : ControllerBase
     }
 
     [HttpGet]
+    [Route("validate-code")]
     [AllowAnonymous]
     public async Task<IActionResult> ValidateCode(string email, long code)
     {
         bool result = await _service.GetCodeByEmail(email, code);
 
         return Ok(result);
+    }
+
+    [HttpPut]
+    [Route("update-password")]
+    [AllowAnonymous]
+    public async Task<IActionResult> Update([FromBody] UpdatePasswordInputModel input)
+    {
+        await _userService.Update(input);
+
+        return Ok();
     }
 }
