5f51e601ab6518ce6fca6796350c8f7266660de4
Guilherme Cal <1083465@sga.pucminas.br>
2024-06-03 21:21:19 -0300

    Alterações visuais home

--- codigo-fonte/Frontend/src/components/DropDown.tsx
+++ codigo-fonte/Frontend/src/components/DropDown.tsx
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
   container: {
     position: 'absolute',
     top: 0,
-    marginTop: '13%',
+    marginTop: '4%',
     right: 0,
     marginRight: '1.5%',
   },
@@ -54,10 +54,10 @@ const styles = StyleSheet.create({
 
   dropdown: {
     position: 'absolute',
-    backgroundColor: '#FFFFFF',
-    width: 150,
-    top: 44,
-    right: -10,
+    backgroundColor: 'rgba(255,255,255, 1)',
+    width: 355,
+    top: 40,
+    right: 10,
     zIndex: 1000,
     borderRadius: 5,
   },
@@ -66,7 +66,6 @@ const styles = StyleSheet.create({
     padding: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#DDD',
-    borderRadius: 5,
   },
 
   menuItemText: {
